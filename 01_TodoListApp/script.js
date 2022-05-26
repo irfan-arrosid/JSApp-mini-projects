aaf0518f798ebddf9e886fc4491eddfc5f25840e
irfan-arrosid <irfanarrosid4@gmail.com>
2022-05-27 00:54:44 +0700

    Add script for showing all task from local storage

--- 01_TodoListApp/script.js
+++ 01_TodoListApp/script.js
@@ -1,6 +1,30 @@
-const taskInput = document.querySelector(".task-input input");
+const taskInput = document.querySelector(".task-input input"),
+    taskBox = document.querySelector(".task-box");
 
-todos = JSON.parse(localStorage.getItem("todo-list"));
+let todos = JSON.parse(localStorage.getItem("todo-list"));
+
+function showTodo() {
+    let li = "";
+    if (todos) {
+        todos.forEach((todo, id) => {
+            li += `<li class="task">
+                     <label for="${id}">
+                         <input type="checkbox" id="${id}">
+                         <p>${todo.name}</p>
+                     </label>
+                     <div class="settings">
+                         <i class="uil uil-ellipsis-h"></i>
+                         <ul class="task-menu">
+                             <li><i class="uil uil-pen"></i>Edit</li>
+                             <li><i class="uil uil-trash"></i>Delete</li>
+                         </ul>
+                     </div>
+                 </li>`;
+        });
+    }
+    taskBox.innerHTML = li;
+}
+showTodo();
 
 taskInput.addEventListener("keyup", e => {
     let userTask = taskInput.value.trim();
@@ -12,5 +36,6 @@ taskInput.addEventListener("keyup", e => {
         let taskInfo = { name: userTask, status: "pending" };
         todos.push(taskInfo);
         localStorage.setItem("todo-list", JSON.stringify(todos));
+        showTodo();
     }
 });
