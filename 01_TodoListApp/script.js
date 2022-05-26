163d87f3186245d6618c717fdfaf9b70cd1ca0b8
irfan-arrosid <irfanarrosid4@gmail.com>
2022-05-26 22:44:47 +0700

    Add script for show all tasks input to local storage

---
+++ 01_TodoListApp/script.js
@@ -0,0 +1,16 @@
+const taskInput = document.querySelector(".task-input input");
+
+todos = JSON.parse(localStorage.getItem("todo-list"));
+
+taskInput.addEventListener("keyup", e => {
+    let userTask = taskInput.value.trim();
+    if (e.key == "Enter" && userTask) {
+        if (!todos) {
+            todos = [];
+        }
+        taskInput.value = "";
+        let taskInfo = { name: userTask, status: "pending" };
+        todos.push(taskInfo);
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+    }
+});
