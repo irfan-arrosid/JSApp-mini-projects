a459b9bb11855aaa238dab3cff02f23502789af7
irfan-arrosid <irfanarrosid4@gmail.com>
2022-05-28 11:44:36 +0700

    Add script for updateStatus and showMenu when click ellipsis

--- 01_TodoListApp/script.js
+++ 01_TodoListApp/script.js
@@ -7,13 +7,14 @@ function showTodo() {
     let li = "";
     if (todos) {
         todos.forEach((todo, id) => {
+            let isCompleted = todo.status == "completed" ? "checked" : "";
             li += `<li class="task">
                      <label for="${id}">
-                         <input type="checkbox" id="${id}">
-                         <p>${todo.name}</p>
+                         <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${isCompleted}>
+                         <p class="${isCompleted}">${todo.name}</p>
                      </label>
                      <div class="settings">
-                         <i class="uil uil-ellipsis-h"></i>
+                         <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
                          <ul class="task-menu">
                              <li><i class="uil uil-pen"></i>Edit</li>
                              <li><i class="uil uil-trash"></i>Delete</li>
@@ -25,6 +26,28 @@ function showTodo() {
     taskBox.innerHTML = li;
 }
 showTodo();
+
+function showMenu(selectedTask) {
+    let taskMenu = selectedTask.parentElement.lastElementChild;
+    taskMenu.classList.add("show");
+    document.addEventListener("click", e => {
+        if (e.target.tagName != "I" || e.target != selectedTask) {
+            taskMenu.classList.remove("show");
+        }
+    })
+}
+
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if (selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+}
 
 taskInput.addEventListener("keyup", e => {
     let userTask = taskInput.value.trim();
