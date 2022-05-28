e38ac244a3e950b1677b4ffbcd6eb070d79ae937
irfan-arrosid <irfanarrosid4@gmail.com>
2022-05-28 12:01:18 +0700

    Add script for delete task

--- 01_TodoListApp/script.js
+++ 01_TodoListApp/script.js
@@ -17,7 +17,7 @@ function showTodo() {
                          <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
                          <ul class="task-menu">
                              <li><i class="uil uil-pen"></i>Edit</li>
-                             <li><i class="uil uil-trash"></i>Delete</li>
+                             <li onclick="deleteTask(${id})"><i class="uil uil-trash"></i>Delete</li>
                          </ul>
                      </div>
                  </li>`;
@@ -37,13 +37,23 @@ function showMenu(selectedTask) {
     })
 }
 
+function deleteTask(deleteId) {
+    // removing selected task from array/todos
+    todos.splice(deleteId, 1);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo();
+}
+
 function updateStatus(selectedTask) {
+    // getting paragraph that contains task name
     let taskName = selectedTask.parentElement.lastElementChild;
     if (selectedTask.checked) {
         taskName.classList.add("checked");
+        // updating the status of selected task to completed
         todos[selectedTask.id].status = "completed";
     } else {
         taskName.classList.remove("checked");
+        // updating the status of selected task to pending
         todos[selectedTask.id].status = "pending";
     }
     localStorage.setItem("todo-list", JSON.stringify(todos));
