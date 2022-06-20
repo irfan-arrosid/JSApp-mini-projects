85d76a424c79fbedab51b555e94dfa683ff32e3a
irfan-arrosid <irfanarrosid4@gmail.com>
2022-06-20 22:08:25 +0700

    Add script for validate email and password when submitting it

--- 04_LoginFormValidation/script.js
+++ 04_LoginFormValidation/script.js
@@ -8,6 +8,8 @@ form.onsubmit = (e) => {
     e.preventDefault; //preventing form from submitting
     if (eInput.value == "") { //if email is empty
         eField.classList.add("shake", "error");
+    } else {
+        checkEmail(); //calling checkEmail function
     }
     if (pInput.value == "") { //if password is empty
         pField.classList.add("shake", "error");
@@ -16,4 +18,34 @@ form.onsubmit = (e) => {
         eField.classList.remove("shake");
         pField.classList.remove("shake");
     }, 500);
+
+    eInput.onkeyup = () => {
+        checkEmail(); //calling checkEmail function
+    }
+
+    function checkEmail() {
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern to validate email
+        if (!eInput.value.match(pattern)) { //if pattern not matched with user entered value
+            eField.classList.add("error");
+            let errorTxt = eField.querySelector(".error-txt");
+            //if email is not empty then show enter a valid email address until email valid else show email can't be blank
+            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address": errorTxt.innerText = "Email can't be blank";
+        } else {
+            eField.classList.remove("error");
+        }
+    }
+
+    pInput.onkeyup = () => {
+        if (pInput.value == "") { //if password is empty
+            pField.classList.add("error");
+        } else {
+            pField.classList.remove("error");
+        }
+    }
+
+    //if error class not contains in eField and pField then user has entered proper details
+    if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+        window.location.href = "#";
+        console.log("Form Submitted!")
+    }
 }
