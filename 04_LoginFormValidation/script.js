eeeef774b28a411273d8c875e50e107c89580f65
irfan-arrosid <irfanarrosid4@gmail.com>
2022-06-20 21:01:33 +0700

    Add script shake animation when email and password is empty

--- 04_LoginFormValidation/script.js
+++ 04_LoginFormValidation/script.js
@@ -1,1 +1,19 @@
-const form = document.querySelector("form");
+const form = document.querySelector("form"),
+    eField = form.querySelector(".email"),
+    eInput = eField.querySelector("input"),
+    pField = form.querySelector(".password"),
+    pInput = pField.querySelector("input");
+
+form.onsubmit = (e) => {
+    e.preventDefault; //preventing form from submitting
+    if (eInput.value == "") { //if email is empty
+        eField.classList.add("shake", "error");
+    }
+    if (pInput.value == "") { //if password is empty
+        pField.classList.add("shake", "error");
+    }
+    setTimeout(() => { //remove shake class after 500ms
+        eField.classList.remove("shake");
+        pField.classList.remove("shake");
+    }, 500);
+}
